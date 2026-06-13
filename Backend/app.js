d3f48b1c23688c948695dcdf453120912c561730
DhamzBonaparte <sulavdhami420@gmail.com>
2026-06-13 13:50:30 +0545

    MAde few changes

--- Backend/app.js
+++ Backend/app.js
@@ -7,16 +7,29 @@ const credentials = require("./Routes/route");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
+const allowedOrigins = [
+  "http://localhost:5173", // local dev
+  "https://futhub.netlify.app", // production frontend
+];
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-console.log(path.join(__dirname, "uploads"))
+console.log(path.join(__dirname, "uploads"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://futhub.netlify.app/",
+    origin: function (origin, callback) {
+      // allow requests with no origin (like curl, mobile apps)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
-); 
+);
 app.use(cookieParser());
 app.use("/api/v1", credentials);
 
@@ -28,7 +41,7 @@ const start = async () => {
     if (await connect(URL)) {
       console.log("Database Connected");
     }
-    app.listen(PORT, () => {  
+    app.listen(PORT, () => {
       console.log("Server connected!");
     });
   } catch (error) {
